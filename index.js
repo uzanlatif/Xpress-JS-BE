1df62edd8e529107ea572ed923cc4490fa85062b
TamanSariGroup <uzanlatif@gmail.com>
2025-03-06 11:26:59 +0700

    template get post put get and connect to db

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const db = require('./connection')
+const response = require('./response')
+
+app.use(bodyParser.json())
 
 //routes get
-app.get('/',(req,res) => {
-    res.send('utama')
+app.get('/',(req, res) => {
+    res.send('utama!')
 })
 
-app.get('/hello',(req,res) => {
-    res.send('Hello panda!')
+app.get('/user',(req, res) => {
+    const sql = "SELECT * FROM user"
+
+    db.query(sql,(error, result) =>{
+        response(200, result, "get all data from user", res)
+    })
 })
 
-app.get('/response',(request,response) => {
-    response.send('response!')
+app.get('/find', (req, res) => {
+    const namaPt = req.query.nama_pt;
+    console.log('find nama_pt: ' + namaPt);
+
+    const sql = 'SELECT nett FROM user WHERE nama_pt = ?';
+
+    db.query(sql, [namaPt], (error, result) => {
+        if (error) {
+            console.error(error);
+            return res.status(500).json({ error: 'Database error' });
+        }
+        response(200, result, "find user", res);
+    });
+});
+
+
+app.post('/login',(req, res) => {
+    console.log({ requestFromOutside: req.body})
+    res.send('login berhasil')
+})
+
+app.put('/username',(req, res) => {
+    console.log({ updateData:req.body})
+    res.send('update put')
 })
 
 app.listen(port,() => {
